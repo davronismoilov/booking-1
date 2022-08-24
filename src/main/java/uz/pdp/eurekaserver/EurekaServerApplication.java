df582559383df1bc7447e954f70ce9b182202e74
davronismoilov <ismoilovdavron16@gmail.com>
2022-08-25 02:52:53 +0500

    deploy

--- src/main/java/uz/pdp/eurekaserver/EurekaServerApplication.java
+++ src/main/java/uz/pdp/eurekaserver/EurekaServerApplication.java
@@ -3,10 +3,20 @@ package uz.pdp.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
+@RequestMapping("test")
 public class EurekaServerApplication {
+
+    @GetMapping
+    public String fun(){
+        return "Jenkins";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
